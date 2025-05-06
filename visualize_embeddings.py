f0607c10435d4d8d2d0f8d188258aef755915beb
sophiashuvar <sophiashuvar@gmail.com>
2025-05-06 14:06:13 +0200

    fix

--- visualize_embeddings.py
+++ visualize_embeddings.py
@@ -15,12 +15,13 @@ from hash import dhash, phash, hash_to_bitvector
 from pretrained import extract_features, initialize_model
 
 SUPPORTED_METHODS = [
-    "dhash", "phash",
-    "pretrained_resnet", "pretrained_mobilenet", "pretrained_efficientnet",
-    "autoencoder_basic", "autoencoder_resnet", "autoencoder_mobilenet", "autoencoder_efficientnet",
-    "siamese_basic", "siamese_resnet", "siamese_mobilenet", "siamese_efficientnet",
-    "siamese_autoencoder_basic",  "siamese_autoencoder_better", "siamese_autoencoder_resnet",
-    "siamese_autoencoder_mobilenet", "siamese_autoencoder_efficientnet", "siamese_better"
+"siamese_better"
+    # "dhash", "phash",
+    # "pretrained_resnet", "pretrained_mobilenet", "pretrained_efficientnet",
+    # "autoencoder_basic", "autoencoder_resnet", "autoencoder_mobilenet", "autoencoder_efficientnet",
+    # "siamese_basic", "siamese_resnet", "siamese_mobilenet", "siamese_efficientnet",
+    # "siamese_autoencoder_basic",  "siamese_autoencoder_better", "siamese_autoencoder_resnet",
+    # "siamese_autoencoder_mobilenet", "siamese_autoencoder_efficientnet", "siamese_better"
 ]
 
 
