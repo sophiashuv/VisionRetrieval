70ddc2671f6a4daee0140ed55146e15716c7ced1
sophiashuvar <sophiashuvar@gmail.com>
2025-05-14 01:36:47 +0200

    fixes

--- test_methods.py
+++ test_methods.py
@@ -122,7 +122,7 @@ def evaluate_retrieval(query_folder, database_folder, save_folder, method, embed
 
         models = method.split("_")
         encoder = build_encoder(models[-1], embedding_dim, None, device)
-        use_head = models[-1] in ["basic", "better"]
+        use_head = models[-1] in ["basic", "better"] and models[-1] != "autoencoder"
         if models[-1] == "mobilenet":
             encoder_output_dim = 1280
         elif models[-1] == "resnet":
@@ -198,7 +198,7 @@ def evaluate_retrieval(query_folder, database_folder, save_folder, method, embed
                     query_vector = query_vector / np.linalg.norm(query_vector)
                     query_vector = query_vector.astype(np.float32).reshape(1, -1)
 
-                distances, indices = index.search(query_vector, 5)
+                distances, indices = index.search(query_vector, 6)
                 retrieved_paths = metadata.iloc[indices[0]]['image_path'].tolist()
                 query_basename = os.path.basename(filename)
                 top_5 = [p for p in retrieved_paths if os.path.basename(p) != query_basename]
